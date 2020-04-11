2f756c075806c1dc7b8517d43012be2011c7bd6e
Namkyung <iddbxk@naver.com>
2020-04-11 13:56:29 +0900

    2020-04-11-am

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import LoginForm from './LoginForm';
 import './css/Header.css';
-import pic from './images/3.jpg';
+import logo from './images/3.jpg';
 function Header() {
+
+const styles_pknu = {
+    color : 'black',
+    fontSize : '70px',
+    textAlign : 'center',
+    float : 'left',
+    width: '500px',
+    height : '100px',
+    textDecoration : 'none'
+}
+
+//const loginStatus = 'undefined';
+
   return (
     <div className="header">
-      <div className="header_left">
-          <img src={pic} width='150px' height='100px' align='left' alt="not photo"></img>
+          <img src={logo} alt="not_photo" width='150px' height='100px' align='left'/>
+          <a style={styles_pknu} href="/">PKNU</a>
+          <div><LoginForm/></div>
       </div>
-    </div>
   );
 }
 
